0050c863b87bdb289a9688c128663c346a499748
Chipili Kafwilo <chipili.kafwilo@advance.io>
2025-04-10 09:27:33 +0200

    chore: Add import for package

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,25 +1,40 @@
-// Do not change this file. Instead add an _index.ts file. That will automatically override this file
+import { Converter } from 'html-to-document';
+
 export const run: () => Promise<any> = async () => {
   const editorContainer = document.getElementById('editor');
-  editorContainer?.append(document.createElement('textarea'));
+  if (!editorContainer) throw new Error('Entrypoint not found');
 
-  // Import TinyMCE dynamically.
+  // Append a textarea for TinyMCE.
+  editorContainer.append(document.createElement('textarea'));
+
+  // Dynamically import TinyMCE.
   const tinymceModule = await import('tinymce');
   const tinymce = tinymceModule.default;
+  const converter = new Converter();
 
+  // Initialize TinyMCE.
   tinymce.init({
     selector: 'textarea',
     height: window.innerHeight - 20,
-    script_url: 'https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js',
-    base_url: 'https://cdn.tiny.cloud/1/no-api-key/tinymce/6',
-    // Add plugins to enable lists and other formatting options.
+    script_url: 'https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js',
+    base_url: 'https://cdn.tiny.cloud/1/no-api-key/tinymce/7',
     plugins: 'lists link paste table code',
-    // Configure the toolbar to expose the buttons for these plugins.
     toolbar:
-      'undo redo | formatselect | bold italic underline | bullist numlist | alignleft aligncenter alignright | link | table | code',
-    setup: (editor: any) => {
+      'undo redo | formatselect | bold italic underline | bullist numlist | alignleft aligncenter alignright | link | table | code | docx',
+    setup: (editor) => {
       editor.on('init', function () {
         console.log('TinyMCE editor is initialized');
+        // Register a custom button on the toolbar named "mydocx".
+        editor.ui.registry.addButton('docx', {
+          icon: 'export-word',
+          text: 'Export word',
+          tooltip: 'Generate DOCX from Editor Content',
+          onAction: async () => {
+            // Get the current content from TinyMCE.
+            const htmlContent = editor.getContent();
+            console.log('HTML Content:', htmlContent);
+          },
+        });
       });
     },
   });
