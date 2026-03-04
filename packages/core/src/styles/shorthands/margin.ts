083da5c00381a2c623b99a78993c6f0026509f6c
Alexander Nortung <alexander.nortung@oakdigital.dk>
2026-03-04 12:54:51 +0100

    wip margin shorthands

---
+++ packages/core/src/styles/shorthands/margin.ts
@@ -0,0 +1,10 @@
+import type * as CSS from 'csstype';
+import { capitalize } from '../../utils/text';
+import { borderStyleValues } from '../../utils/html.utils';
+import { directions } from '../directions';
+
+export const expandMarginShorthand = (
+  mappedStyles: Partial<Record<keyof CSS.Properties, string | number>>
+) => {
+  //
+};
