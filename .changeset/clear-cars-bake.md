61f68e77987f5d84ebe9511af801870f0a326c3f
Alexander Nortung <alexander.nortung@oakdigital.dk>
2026-04-29 12:38:27 +0200

    feat(css-parser-plugin): added plugin

---
+++ .changeset/clear-cars-bake.md
@@ -0,0 +1,6 @@
+---
+'html-to-document-plugin-css-parser': minor
+'html-to-document': minor
+---
+
+Added new CSS parser plugin
--- packages/html-to-document/src/index.ts
+++ packages/html-to-document/src/index.ts
@@ -1,5 +1,6 @@
 export * from 'html-to-document-core';
 export { DocxAdapter } from 'html-to-document-adapter-docx';
+export { cssParserPlugin } from 'html-to-document-plugin-css-parser';
 export type {
   DocxAdapterConfig,
   DocxElement,
